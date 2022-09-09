ad60eb48fb6fe6bdb76d44bec737884101cd5422
LeoLeeYM <52209163+LeoLeeYM@users.noreply.github.com>
2022-09-09 17:40:22 +0800

    Delete test.py

--- PEL/Script/test.py
+++
@@ -1,7 +0,0 @@
-import commands
-
-scpython = "H:\\SystemFile\\Desktop\\PyE-Lang\\PEL\\Script\\SCPythonForPEL\\SCPython.py"
-testscript = "H:\\SystemFile\\Desktop\\PyE-Lang\\PEL\\Script\\新建文本文档.txt"
-print(f"python {scpython} {testscript}")
-a = commands.getoutput(f"python {scpython} {testscript}")
-print()
